be03063267cde05739516112ff6050f066cc2282
name <2282697584@qq.com>
2021-06-04 09:27:05 +0800

    update

---
+++ takeOutPro/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+        <van-nav-bar title="标题" left-text="返回" class="nav" left-arrow>
+        <template #left>
+        <van-icon name="search" color="#FFFFFF" size="18" />
+        </template>
+        </van-nav-bar>
+        </div>
+        <div>
+        <van-tabbar v-model="active">
+        <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+        <van-tabbar-item icon="search">搜索</van-tabbar-item>
+        <van-tabbar-item icon="shopping-cart-o">订单</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">我的</van-tabbar-item>
+        </van-tabbar>
+            
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    name:"home",
+    data(){
+        return{
+            active:0
+        }
+    }
+}
+</script>
